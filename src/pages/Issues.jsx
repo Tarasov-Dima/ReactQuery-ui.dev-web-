4b0239bf1b8dd60556eaf789069a4f1a1934ff85
Dima <dmytrotarasov9@gmail.com>
2023-05-21 18:26:35 +0200

    feat(Issues): filter by status

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 import IssuesList from "../components/IssuesList";
 import LabelList from "../components/LabelList";
+import { StatusSelect } from "./StatusSelect";
 export default function Issues() {
 	const [labels, setLabels] = useState([]);
+	const [status, setStatus] = useState("");
 
-	const setCurrentLabels = (label) => {
+	function setCurrentLabels(label) {
 		setLabels((currentLabels) =>
 			currentLabels.includes(label)
 				? currentLabels.filter((currentLabel) => currentLabel !== label)
 				: currentLabels.concat(label)
 		);
-	};
+	}
+	function setCurrentStatus(event) {
+		setStatus(event.target.value);
+	}
 	return (
 		<div>
 			<main>
 				<section>
 					<h1>Issues</h1>
-					<IssuesList labels={labels} />
+					<IssuesList labels={labels} status={status} />
 				</section>
 				<aside>
 					<LabelList selected={labels} toggle={setCurrentLabels} />
+					<h3>Status</h3>
+					<StatusSelect value={status} onChange={setCurrentStatus} />
 				</aside>
 			</main>
 		</div>
