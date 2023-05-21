321e9f2c0d7922c1c9cf1fa963bcdcb916addfef
Dima <dmytrotarasov@odrabiamy.pl>
2023-05-21 16:24:57 +0200

    feat(LabelList): added items to label list

--- src/components/LabelList.jsx
+++ src/components/LabelList.jsx
@@ -1,3 +1,29 @@
-export default function LabelList() {
-  return <h3>Labels</h3>;
+import { useLabelsData } from "../helpers/useLabelsData";
+
+export default function LabelList({ selected, toggle }) {
+	const labelsQuery = useLabelsData();
+
+	const renderLabels = () => {
+		return labelsQuery.data.map((label) => {
+			return (
+				<li key={label.id}>
+					<button
+						className={`label ${
+							selected.includes(label.id) ? "selected" : ""
+						} ${label.color} `}
+						onClick={() => toggle(label.id)}
+					>
+						{label.name}
+					</button>
+				</li>
+			);
+		});
+	};
+
+	return (
+		<div className='labels'>
+			<h3>Labels</h3>
+			{labelsQuery.isLoading ? <p>loading...</p> : <ul>{renderLabels()}</ul>}
+		</div>
+	);
 }
